d638e272f51e60f2522dd9782446b15fcb4e554c
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2023-01-16 20:41:39 +0300

    setup database

---
+++ schema.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE animals (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(60) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT NOT NULL,
+    neutered BOOLEAN NOT NULL,
+    weight_kg DECIMAL NOT NULL
+);
